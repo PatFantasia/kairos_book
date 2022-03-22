5df1ddb64b1e3b7cc48d7d791edfe46d02870ce7
PatFantasia <desouza.paatrice@gmail.com>
2022-03-22 12:01:44 +0100

    feat: Add sing up page #1

--- src/navigation/Main.js
+++ src/navigation/Main.js
@@ -1,48 +1,60 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack';
-import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 
-import {Profile} from '../screens/';
-import Tabs from './Tabs';
-import {Profile} from '../screens/';
-import {OnboardingScreen} from '../screens/';
+import Tabs from "./Tabs";
+import { Profile } from "../screens/";
+import { OnboardingScreen } from "../screens/";
+import SingUp from "../screens/SingUp";
 
 const Stack = createStackNavigator();
 
 const Main = () => {
-    const getHeaderTitle = route => {
-        const routeName = getFocusedRouteNameFromRoute(route)??  'Bibliothèque';
-      const defaultName = getFocusedRouteNameFromRoute(route) ==='Tabs'? 'Bibliothèque':'null';
-        const routeName = (getFocusedRouteNameFromRoute(route)??  'Bibliothèque') || defaultName ;
-        
-        const renameRoutes = {
-          News : "Aujourd'hui",
-          Library : "Bibliothèque",
-          Bookstore : "Librairie",
-          Audiobook : "Livres Audio",
-          Search : "Rechercher"
-        }
-        return renameRoutes[routeName] ;
-      }
-      
+  const getHeaderTitle = (route) => {
+    const defaultName =
+      getFocusedRouteNameFromRoute(route) === "Tabs" ? "Bibliothèque" : "null";
+    const routeName =
+      (getFocusedRouteNameFromRoute(route) ?? "Bibliothèque") || defaultName;
+
+    const renameRoutes = {
+      News: "Aujourd'hui",
+      Library: "Bibliothèque",
+      Bookstore: "Librairie",
+      Audiobook: "Livres Audio",
+      Search: "Rechercher",
+    };
+    return renameRoutes[routeName];
+  };
+
   return (
-      <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} options={{headerShown: false}} />
-            <Stack.Screen 
-                name="Tabs" 
-                component={Tabs}
-                options={({ route }) => ({
-                    headerTitle: getHeaderTitle(route),
-                  })} 
-            
-            />
-            <Stack.Screen name="Profile" component={Profile} options={{title: "Profile"}} />
-        </Stack.Navigator>
-      </NavigationContainer>
-  )
-}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="OnboardingScreen"
+          component={OnboardingScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SingUp"
+          component={SingUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Tabs"
+          component={Tabs}
+          options={({ route }) => ({
+            headerTitle: getHeaderTitle(route),
+          })}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ title: "Profile" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default Main;
-
--- src/navigation/Tabs.js
+++ src/navigation/Tabs.js
@@ -1,73 +1,85 @@
-import React from 'react';
-import {StyleSheet, Button} from 'react-native'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { getHeaderTitle } from '@react-navigation/elements';
-import {BlurView} from 'expo-blur';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { Icon } from 'react-native-elements';
+import { Icon } from "react-native-elements";
 
-import {News} from '../screens';
-import {Library} from '../screens';
-import {Bookstore} from '../screens';
-import {Audiobook} from '../screens';
-import {Search} from '../screens';
+import { News } from "../screens";
+import { Library } from "../screens";
+import { Bookstore } from "../screens";
+import { Audiobook } from "../screens";
+import { Search } from "../screens";
 
 const Tab = createBottomTabNavigator();
 const config = {
-  News : { name:"article", type:"materialicons"},
-  Library : { name:"local-library", type:"materialicons"},
-  Bookstore : { name:"storefront", type:"materialcommunityicons"},
-  Audiobook : { name:"audiotrack", type:"materialicons"},
-  Search : { name:"search", type:"ionicons"},
-}
+  News: { name: "article", type: "materialicons" },
+  Library: { name: "local-library", type: "materialicons" },
+  Bookstore: { name: "storefront", type: "materialcommunityicons" },
+  Audiobook: { name: "audiotrack", type: "materialicons" },
+  Search: { name: "search", type: "ionicons" },
+};
 
 const Tabs = () => {
   return (
     <Tab.Navigator
       initialRouteName="Library"
-      screenOptions= {({route}) => ({
-          headerShown: false,
-          tabBarIcon: ({focused}) => (
-            <Icon 
-              name = {config[route.name].name}
-              type = {config[route.name].type}
-              color = {focused? "blue" : "grey"}
-            />
-          ),
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <Icon
+            name={config[route.name].name}
+            type={config[route.name].type}
+            color={focused ? "blue" : "grey"}
+          />
+        ),
 
-          tabBarStyle: {position: "absolute"},
-          tabBarActiveTintColor : "grey",
-          tabBarStyle: {
-            position: 'absolute',
-            backgroundColor: 'transparent',
-          },
-          tabBarHideOnKeyboard: true,
-          tabBarItemStyle: {
-            backgroundColor: "transparent",
-            borderColor: "red"
-          },
-          tabBarIconStyle: {
-            backgroundColor: "transparent",
-            borderColor: "red"
-          },
-          tabBarLabelStyle: {
-            backgroundColor: "transparent",
-            backfaceVisibility: "hidden",
-            borderColor: "red"
-          }, 
-
-       
-        })
-      }
+        tabBarActiveTintColor: "grey",
+        tabBarStyle: {
+          position: "absolute",
+          backgroundColor: "transparent",
+        },
+        tabBarHideOnKeyboard: true,
+        tabBarItemStyle: {
+          backgroundColor: "transparent",
+          borderColor: "red",
+        },
+        tabBarIconStyle: {
+          backgroundColor: "transparent",
+          borderColor: "red",
+        },
+        tabBarLabelStyle: {
+          backgroundColor: "transparent",
+          backfaceVisibility: "hidden",
+          borderColor: "red",
+        },
+      })}
     >
-        <Tab.Screen name="News" component={News} options={{title: "Aujourd'hui"}} />
-        <Tab.Screen name="Library" component={Library} options={{title: "Bibliothèque"}} />
-        <Tab.Screen name="Bookstore" component={Bookstore} options={{title: "Librairie"}} />
-        <Tab.Screen name="Audiobook" component={Audiobook} options={{title: "Livres Audio"}} />
-        <Tab.Screen name="Search" component={Search} options={{title: "Recherche"}} />
+      <Tab.Screen
+        name="News"
+        component={News}
+        options={{ title: "Aujourd'hui" }}
+      />
+      <Tab.Screen
+        name="Library"
+        component={Library}
+        options={{ title: "Bibliothèque" }}
+      />
+      <Tab.Screen
+        name="Bookstore"
+        component={Bookstore}
+        options={{ title: "Librairie" }}
+      />
+      <Tab.Screen
+        name="Audiobook"
+        component={Audiobook}
+        options={{ title: "Livres Audio" }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{ title: "Recherche" }}
+      />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
 export default Tabs;
-
