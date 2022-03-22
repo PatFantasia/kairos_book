cbe021d9bb21ce104ac55ad6d802d840e8a28e02
PatFantasia <desouza.paatrice@gmail.com>
2022-03-22 08:17:42 +0100

    feat: Add assets and config for the onboarding screen #1

---
+++ src/constants/images.js
@@ -0,0 +1,10 @@
+export const onboardingPicture_one = require("../../assets/images/onboardingPicture_one.png");
+export const onboardingPicture_two = require("../../assets/images/onboardingPicture_two.png");
+export const onboardingPicture_three = require("../../assets/images/onboardingPicture_three.png");
+
+export default {
+    onboardingPicture_one,
+    onboardingPicture_two,
+    onboardingPicture_three
+    
+}
