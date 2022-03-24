ce75f85359037abc82b3a5d8f23b80251abeca49
PatFantasia <desouza.paatrice@gmail.com>
2022-03-24 10:33:03 +0100

    feat: Configure files to match with linter #3

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,31 @@
+/* eslint-disable no-undef */
+
 export const onboardingPicture_one = require("../../assets/images/onboardingPicture_one.png");
 export const onboardingPicture_two = require("../../assets/images/onboardingPicture_two.png");
 export const onboardingPicture_three = require("../../assets/images/onboardingPicture_three.png");
+export const otherWordsForHome = require("../../assets/images/other_words_for_home.jpg");
+export const theMetropolist = require("../../assets/images/the_metropolist.jpg");
+export const theTinyDragon = require("../../assets/images/the_tiny_dragon.jpg");
+export const underland = require("../../assets/images/underland.jpg");
+export const immigritude = require("../../assets/images/immigritude.jpg");
+export const j_ai_peche = require("../../assets/images/j_ai_peche.jpg");
+export const le_mal_d_aimer = require("../../assets/images/le_mal_d_aimer.jpg");
+export const les_serveuses_de_fatasmes = require("../../assets/images/les_serveuses_de_fatasmes.jpg");
+export const plus_de_noel_pour_etewutulu = require("../../assets/images/plus_de_noel_pour_etewutu.jpg");
+export const tremblement_de_corps = require("../../assets/images/tremblement_de_corps.jpg");
 
 export default {
   onboardingPicture_one,
   onboardingPicture_two,
   onboardingPicture_three,
+  otherWordsForHome,
+  theMetropolist,
+  theTinyDragon,
+  underland,
+  immigritude,
+  j_ai_peche,
+  le_mal_d_aimer,
+  les_serveuses_de_fatasmes,
+  plus_de_noel_pour_etewutulu,
+  tremblement_de_corps,
 };
