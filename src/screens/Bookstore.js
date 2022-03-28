4eaa6856a4ca458b72b09e79bb70db1ab2d9dcdd
PatFantasia <desouza.paatrice@gmail.com>
2022-03-28 21:31:28 +0200

    feat: Add Bookstore screen basic set up #1

--- src/screens/Bookstore.js
+++ src/screens/Bookstore.js
@@ -1,37 +1,67 @@
-import React from "react";
-import { Text, View, SafeAreaView, TouchableOpacity } from "react-native";
-import { Icon, Divider } from "react-native-elements";
-import { COLORS, FONTS, SIZES } from "../constants/themes";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  ScrollView,
+} from "react-native";
+import { Divider } from "react-native-elements";
+import { COLORS } from "../constants/themes";
+// import dummyData from "constants/dummyData";
+import {
+  myBooksData,
+  categoriesData,
+  fancyCategoriesData,
+} from "constants/dummyData";
+import RenderFancyContent from "components/RenderFancyContent";
+import DisplaySection from "components/DisplaySection";
+import DisplayAll from "components/DisplayAll";
+import RenderTrendContent from "components/RenderTrendContent";
+import RenderByGenre from "components/RenderByGenre";
+import SeeMore from "components/SeeMore";
 
-const Bookstore = () => {
-  const displayListOfTopics = () => {
-    return (
-      <TouchableOpacity
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          paddingHorizontal: SIZES.padding,
-          top: -70,
-          alignItems: "center",
-          // alignSelf: "flex-start",
-        }}
-      >
-        <Icon name="menu" type="feather" color={COLORS.white} />
-        <Text style={{ ...FONTS.h3, color: COLORS.white }}>
-          {" "}
-          Parcourir les sections{" "}
-        </Text>
-      </TouchableOpacity>
-    );
-  };
-
+const Bookstore = ({ navigation }) => {
+  const [bookData, setBookData] = useState(myBooksData);
+  const [fancyCategories, setFancyCategories] = useState(fancyCategoriesData);
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.black }}>
-      <Divider style={{ marginHorizontal: 15 }} color={COLORS.lightGray} />
-      <View style={{ height: 200 }}>{displayListOfTopics()}</View>
-      <Divider style={{ marginHorizontal: 15 }} color={COLORS.lightGray} />
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={{ height: 200 }}>
+          <Divider style={{ marginHorizontal: 15 }} color={COLORS.lightGray} />
+          <DisplaySection />
+        </View>
+
+        <View style={{ height: 380, top: -150 }}>
+          <RenderFancyContent data={fancyCategories} />
+        </View>
+
+        <View style={{ height: 550, top: -150 }}>
+          <RenderTrendContent data={bookData} />
+        </View>
+
+        <View style={{ height: 200, top: -150 }}>
+          <Divider style={{ marginHorizontal: 15 }} color={COLORS.lightGray} />
+          <DisplayAll />
+        </View>
+
+        <View style={{ height: 550, top: -150 }}>
+          <RenderByGenre data={bookData} />
+        </View>
+
+        <View style={{ top: -320 }}>
+          <SeeMore data={bookData} />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 export default Bookstore;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.black,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+});
