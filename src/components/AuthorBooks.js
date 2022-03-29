90b1ee6add82f61f91d7143fdc67bd8ded0150ca
PatFantasia <desouza.paatrice@gmail.com>
2022-03-29 17:13:03 +0200

    feat: Add some micro compoment for Library Item Content #1

---
+++ src/components/AuthorBooks.js
@@ -0,0 +1,178 @@
+import React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Image,
+  ScrollView,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Icon, Divider } from "react-native-elements";
+
+import { COLORS, SIZES } from "../constants/themes";
+
+const AuthorBooks = ({ data, title }) => {
+  const header = () => (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-around",
+        bottom: 5,
+      }}
+    >
+      <TouchableOpacity style={{ marginLeft: -45 }}>
+        <Text
+          style={{
+            color: COLORS.white,
+            fontSize: SIZES.h3,
+            fontWeight: "bold",
+            textAlign: "left",
+          }}
+        >
+          {" "}
+          {title}{" "}
+        </Text>
+        <Text
+          style={{
+            color: COLORS.white,
+            fontSize: SIZES.h3,
+            fontWeight: "bold",
+            textAlign: "left",
+          }}
+        >
+          {" "}
+          {data[0].author}{" "}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{ flexDirection: "row", marginRight: -35 }}>
+        <Text
+          style={{ color: COLORS.white, marginLeft: -12, fontSize: SIZES.h3 }}
+        >
+          {" "}
+          Tout afficher{" "}
+        </Text>
+        <Icon
+          name="keyboard-arrow-right"
+          size={25}
+          color={COLORS.lightGray}
+          style={{ marginLeft: -2 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+  const footer = (item) => (
+    <View
+      style={{
+        justifyContent: "space-between",
+        height: 60,
+      }}
+    >
+      <Text
+        style={{
+          color: COLORS.white,
+          fontSize: SIZES.h4,
+          fontWeight: "bold",
+          textAlign: "left",
+        }}
+      >
+        {" "}
+        {item.bookName}{" "}
+      </Text>
+      <Text
+        style={{
+          color: COLORS.lightGray,
+          fontSize: SIZES.h4,
+          fontWeight: "bold",
+          textAlign: "left",
+        }}
+      >
+        {" "}
+        {data[0].author}{" "}
+      </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          borderRadius: 25,
+          borderWidth: 1,
+          width: 100,
+          borderColor: COLORS.white,
+        }}
+      >
+        <Text style={{ color: COLORS.white, fontSize: SIZES.h3 }}>
+          {" "}
+          {item.price}
+          {" FCFA "}
+        </Text>
+      </View>
+    </View>
+  );
+
+  const renderSection = (data) => {
+    const renderContent = ({ item }) => (
+      <TouchableOpacity
+        style={{
+          marginLeft: SIZES.padding,
+          borderTopColor: COLORS.lightGray,
+        }}
+        // onPress={() =>
+        //   navigation.navigate("FancyContent", {
+        //     content: item,
+        //   })
+        // }
+      >
+        <Image
+          source={item.bookCover}
+          resizeMode="cover"
+          style={{
+            width: 180,
+            height: 250,
+            borderRadius: 5,
+            marginVertical: 15,
+            justifyContent: "center",
+            alignSelf: "center",
+          }}
+          blurRadius={5}
+        />
+        {footer(item)}
+      </TouchableOpacity>
+    );
+    return (
+      <>
+        <View
+          style={{
+            marginLeft: 5,
+            marginTop: 30,
+            // marginBottom: 5,
+            height: 30,
+            // width: 310,
+          }}
+        >
+          {header()}
+        </View>
+        <FlatList
+          data={data}
+          renderItem={renderContent}
+          keyExtractor={(item) => item.id}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          extraData={data}
+        />
+      </>
+    );
+  };
+  return renderSection(data);
+};
+
+export default AuthorBooks;
+
+const styles = StyleSheet.create({
+  primaryTitle: {
+    color: COLORS.whiteSmooth,
+    fontSize: SIZES.h2,
+    fontWeight: "bold",
+    textAlign: "left",
+  },
+});
---
+++ src/components/MetaInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, ScrollView } from "react-native";
+
+import MetaInfoDetails from "./MetaInfoDetails";
+import { SIZES } from "../constants/themes";
+
+const MetaInfo = ({ book }) => {
+  const { genre, release_date, language, pageNo, publishing_house, size } =
+    book;
+  const renderContent = () => (
+    <View style={{ flexDirection: "row" }}>
+      <MetaInfoDetails top={"GENRE"} icon bottom={genre} />
+      <MetaInfoDetails
+        top={"SORTIE"}
+        middle={release_date.year}
+        bottom={`${release_date.date} ${release_date.month}`}
+      />
+      <MetaInfoDetails
+        top={"LANGUE"}
+        middle={language}
+        bottom={language === "Fr" ? "français" : "Anglais"}
+      />
+      <MetaInfoDetails top={"PAGES"} middle={pageNo} bottom={"pages"} />
+      <MetaInfoDetails
+        top={"EDITIONS"}
+        middle={publishing_house}
+        middleCustomStyle={{ fontSize: SIZES.h4, paddingTop: 5 }}
+      />
+      <MetaInfoDetails top={"TAILLE"} middle={size} bottom={"Mo"} />
+    </View>
+  );
+  return (
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      <View>{renderContent()}</View>
+    </ScrollView>
+  );
+};
+
+export default MetaInfo;
