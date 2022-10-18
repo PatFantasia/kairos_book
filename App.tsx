70666b2bd11a0c5f57507dde1afada30197aa53e
PatFantasia <desouza.paatrice@gmail.com>
2022-10-18 15:28:38 +0100

    fix: Fix reactotron debuging problem #4

--- App.tsx
+++ App.tsx
@@ -7,15 +7,20 @@ import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "stores/store";
 import Authentification from "./src/navigation/Authentification";
 import Main from "./src/navigation/Main";
+import reactotron from "./ReactotronConfig";
 
 if (__DEV__) {
   import("./ReactotronConfig").then(() => console.log("Reactotron Configured"));
+
+  console.log = reactotron.log;
+  console.warn = reactotron.warn;
+  console.error = reactotron.error;
 }
 Reactotron.log("Connected");
 
 const App = () => {
   const isFirstTime = false;
-  const isLoggedIn = true;
+  const isLoggedIn = false;
 
   return (
     <Provider store={store}>
--- src/stores/store.js
+++ src/stores/store.js
@@ -4,6 +4,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import autoMergeLevel1 from "redux-persist/lib/stateReconciler/autoMergeLevel1";
 import ExpoFileSystemStorage from "redux-persist-expo-filesystem";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import authReducer from "./slices/authSlice";
 import reactotron from "../../ReactotronConfig";
 
@@ -15,12 +17,14 @@ const persistConfig = {
   storage: ExpoFileSystemStorage,
   stateReconciler: autoMergeLevel1,
 };
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const enhancers = [reactotron.createEnhancer()];
+const composedEnhancers = composeWithDevTools(...enhancers);
 
 export const store = configureStore({
   reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== "production",
-  enhancers: reactotron.createEnhancer(),
+  enhancers: composedEnhancers,
 });
 export const persistor = persistStore(store);
