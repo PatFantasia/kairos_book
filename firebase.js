c7f3df59d6a2ef9a24e895a8927abe8cd69dca76
PatFantasia <desouza.paatrice@gmail.com>
2022-10-19 19:25:19 +0100

    feat: Change firebase basic configuration #4

--- firebase.js
+++ firebase.js
@@ -1,28 +1,26 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { enableIndexedDbPersistence } from "firebase/firestore";
+import * as firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCWBmdRzK3VtSSKt9ue3h-KKnPucrv5WZI",
   authDomain: "kairosbook-d34e1.firebaseapp.com",
   projectId: "kairosbook-d34e1",
-  databaseUrl: "https://kairosbook-d34e1.firebaseio.com",
+  databaseUrl: "kairosbook-d34e1.firebaseio.com",
   storageBucket: "kairosbook-d34e1.appspot.com",
   messagingSenderId: "414773573757",
   appId: "1:414773573757:web:74f27800b59f5765cf9914",
   measurementId: "G-QHP2ZQ3GQV",
 };
 
-const app = initializeApp(firebaseConfig);
+let app;
 
-export const database = getFirestore(app);
+if (!firebase.apps.length) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
 
-enableIndexedDbPersistence(database).catch((err) => {
-  if (err.code == "failed-precondition") {
-    console.log({ err });
-  } else if (err.code == "unimplemented") {
-    console.log({ err });
-  }
-});
+const auth = firebase.auth();
 
-export default app;
+const database = app.firestore();
+
+export { firebaseConfig, app, auth, database };
